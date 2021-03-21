5bcf5d9a5d119a9aa48a05c26477f1272e4a9bfa
nadejda-dmitrenko <nadyadmitrenko@gmail.com>
2021-03-21 13:37:23 +0200

    Added new sout.

--- src/main/java/com/classes/hillel/Main.java
+++ src/main/java/com/classes/hillel/Main.java
@@ -5,5 +5,6 @@ package main.java.com.classes.hillel;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from git!");
+        System.out.println("second message");
     }
 }
